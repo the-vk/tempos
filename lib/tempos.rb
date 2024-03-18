6247834a7bf66b4c565c5de79692841d45c39917
Andrew Maraev <the_vk@thevk.net>
2024-03-17 19:06:32 -0700

    add barebone gem

---
+++ lib/tempos.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tempos/version"
+
+module Tempos
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tempos/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tempos
+  VERSION = "0.1.0"
+end
